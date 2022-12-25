52170c5ca0ec3dd22a51cdd0dc09ee933564d0c5
daras <d.daraselia@gmail.com>
2022-12-25 22:50:09 +0400

    unoptimized: true

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
