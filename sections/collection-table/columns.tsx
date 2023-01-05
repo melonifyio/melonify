ec8825427222fe8fd8dba6b00e99d6c9540d3b67
daras <d.daraselia@gmail.com>
2023-01-05 14:53:18 +0400

    auto column width and flex

--- sections/collection-table/columns.tsx
+++ sections/collection-table/columns.tsx
@@ -2,6 +2,18 @@ import { GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
 import { FieldProps } from "../../components/form-field/types";
 import { TableField } from "../../components/table-field/table-field";
 
+const getColumnWidth = (columnType: string): number => {
+  if (columnType === "IMAGE") return 96;
+
+  return 150;
+};
+
+const getColumnFlex = (columnType: string): number => {
+  if (columnType === "TEXT") return 1;
+
+  return 0;
+};
+
 export const columns = (model: {
   fields: Record<string, FieldProps & { index: number }>;
 }): GridColDef[] => {
@@ -13,8 +25,8 @@ export const columns = (model: {
   const transformedColumns = fieldKeysSorted.map((fieldKey) => ({
     field: model.fields[fieldKey].fieldKey,
     headerName: model.fields[fieldKey].name,
-    width: 150,
-    flex: 1,
+    width: getColumnWidth(model.fields[fieldKey].type),
+    flex: getColumnFlex(model.fields[fieldKey].type),
     renderCell: (params: GridRenderCellParams<any>) => {
       return (
         <TableField type={model.fields[fieldKey].type} value={params.value} />
