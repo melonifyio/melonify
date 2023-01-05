f2f835290a69bdda6376fc76e56e360cfa3afaee
daras <d.daraselia@gmail.com>
2023-01-05 16:01:55 +0400

    ts styles error fix

--- theme/overrides/card.tsx
+++ theme/overrides/card.tsx
@@ -7,37 +7,36 @@ export default function Card(theme: Theme) {
         root: {
           boxShadow: theme.shadows[10],
           borderRadius: Number(theme.shape.borderRadius) * 2,
-          position: "relative",
           zIndex: 0, // Fix Safari overflow: hidden with border radius
         },
       },
-      variants: [
-        {
-          props: { variant: "dashed" },
-          style: {
-            textTransform: "none",
-            border: `2px dashed grey`,
-          },
-        },
-      ],
+      // variants: [
+      //   {
+      //     props: { variant: "dashed" },
+      //     style: {
+      //       textTransform: "none",
+      //       border: `2px dashed grey`,
+      //     },
+      //   },
+      // ],
     },
-    MuiCardHeader: {
-      defaultProps: {
-        titleTypographyProps: { variant: "h6" },
-        subheaderTypographyProps: { variant: "body2" },
-      },
-      styleOverrides: {
-        root: {
-          padding: theme.spacing(3, 3, 0),
-        },
-      },
-    },
-    MuiCardContent: {
-      styleOverrides: {
-        root: {
-          padding: theme.spacing(3),
-        },
-      },
-    },
+    // MuiCardHeader: {
+    //   defaultProps: {
+    //     titleTypographyProps: { variant: "h6" },
+    //     subheaderTypographyProps: { variant: "body2" },
+    //   },
+    //   styleOverrides: {
+    //     root: {
+    //       padding: theme.spacing(3, 3, 0),
+    //     },
+    //   },
+    // },
+    // MuiCardContent: {
+    //   styleOverrides: {
+    //     root: {
+    //       padding: theme.spacing(3),
+    //     },
+    //   },
+    // },
   };
 }
