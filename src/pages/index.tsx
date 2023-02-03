fae77e3c584966e90ff783e9181dc92e363fbd36
daras <d.daraselia@gmail.com>
2023-02-03 23:41:22 +0400

    remove firebase status section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <Container>
       <PageHeader title="Dashboard" />
-      <FirebaseStatus />
+      {/* <FirebaseStatus /> */}
     </Container>
   );
 }
